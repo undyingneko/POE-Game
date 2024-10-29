7f62b182ab2d5f9d17921ea59decaf56aaae5f4e
fenryshu <dongoctram1404@gmail.com>
2024-10-29 17:16:24 +0100

    7_UI Manager

---
+++ POE Like Game/Assets/UIPanelManager.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIPanelManager : MonoBehaviour
+{
+    [SerializeField] GameObject inventoryPanel;
+    [SerializeField] GameObject statsPanel;
+    [SerializeField] GameObject questPanel;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            OpenInventory();
+        }
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            OpenQuestPanel();
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            OpenStats();
+        }
+
+           
+    }
+
+    public void OpenInventory()
+    {
+        inventoryPanel.SetActive(!inventoryPanel.activeInHierarchy);
+    }
+    public void OpenStats()
+    {
+        statsPanel.SetActive(!statsPanel.activeInHierarchy);
+        questPanel.SetActive(false);
+    }
+    public void OpenQuestPanel()
+    {
+        questPanel.SetActive(!questPanel.activeInHierarchy);
+        statsPanel.SetActive(false);
+    }
+
+
+
+}
+
+
