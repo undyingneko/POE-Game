b2d8f89a06ce814e61726d69c74986e2b3465772
fenryshu <dongoctram1404@gmail.com>
2024-10-30 16:47:47 +0100

    14_Grid-Inventory-mycode

--- POE Like Game/Assets/Scripts/InventoryController.cs
+++ POE Like Game/Assets/Scripts/InventoryController.cs
@@ -5,15 +5,50 @@ using UnityEngine;
 public class InventoryController : MonoBehaviour
 {
     [HideInInspector] public ItemGrid selectedItemGrid;
+    Vector2Int positionOnGrid;
+    InventoryItem selectedItem;
+    RectTransform selectedItemRectTransform;
+
     private void Update()
     {
+        // Update the position of the selected item to follow the mouse
+        if (selectedItem != null)
+        {
+            if (selectedItemRectTransform == null)
+            {
+                selectedItemRectTransform = selectedItem.GetComponent<RectTransform>();
+            }
+            selectedItemRectTransform.position = Input.mousePosition;
+        }
+
+        // If there's no grid selected, exit the Update loop
         if (selectedItemGrid == null) { return; }
 
+        // Handle mouse click for picking up or placing items
         if (Input.GetMouseButtonDown(0))
         {
-            Debug.Log(selectedItemGrid.GetTileGridPosition(Input.mousePosition));
+            positionOnGrid = selectedItemGrid.GetTileGridPosition(Input.mousePosition);
+
+            // If no item is selected, attempt to pick up an item
+            if (selectedItem == null)
+            {
+                selectedItem = selectedItemGrid.PickUpItem(positionOnGrid);
+                if (selectedItem != null)
+                {
+                    selectedItemRectTransform = selectedItem.GetComponent<RectTransform>();
+                }
+                else
+                {
+                    Debug.LogWarning("No item found at the grid position to pick up.");
+                }
+            }
+            else
+            {
+                // If an item is selected, attempt to place it in the grid
+                selectedItemGrid.PlaceItem(selectedItem, positionOnGrid.x, positionOnGrid.y);
+                selectedItem = null;
+                selectedItemRectTransform = null;
+            }
         }
     }
-
-
 }
--- POE Like Game/Assets/Scripts/ItemGrid.cs
+++ POE Like Game/Assets/Scripts/ItemGrid.cs
@@ -1,22 +1,64 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class ItemGrid : MonoBehaviour
 {
+    InventoryItem[,] inventoryItemGrid;
     const float TileSizeWidth = 32f;
     const float TileSizeHeight = 32f;
-
+    [SerializeField] int gridSizeWidth;
+    [SerializeField] int gridSizeHeight;
     RectTransform rectTransform;
 
     Vector2 mousePositionOnTheGrid;
     Vector2Int tileGridPosition = new Vector2Int();
-
+    [SerializeField] GameObject inventoryItemPrefab;
 
     private void Awake()
     {
         rectTransform = GetComponent<RectTransform>();
     }
+
+    private void Start()
+    {
+        Init(gridSizeWidth, gridSizeHeight);
+        CreateTestItem(7, 0);
+        CreateTestItem(1, 0);
+        CreateTestItem(2, 2);
+
+    }
+
+    private void CreateTestItem(int x, int y)
+    {
+        GameObject itemTest = Instantiate(inventoryItemPrefab);
+        InventoryItem inventoryItemTest = itemTest.GetComponent<InventoryItem>();
+        PlaceItem(inventoryItemTest, x, y);
+    }
+
+    private void Init(int width, int height)
+    {
+        inventoryItemGrid = new InventoryItem[width, height];
+        Vector2 size = new Vector2();
+        size.x = TileSizeWidth * width;
+        size.y = TileSizeHeight * height;
+        rectTransform.sizeDelta = size;
+    }
+
+    public void PlaceItem(InventoryItem itemToPlace, int x, int y)
+    {
+        RectTransform itemRectTransform = itemToPlace.GetComponent<RectTransform>();
+        itemRectTransform.SetParent(transform);
+
+        inventoryItemGrid[x, y] = itemToPlace;
+
+        Vector2 positionOnGrid = new Vector2();
+        positionOnGrid.x = TileSizeWidth * x + TileSizeWidth / 2;
+        positionOnGrid.y = -(TileSizeHeight * y + TileSizeHeight / 2);
+        itemRectTransform.localPosition = positionOnGrid;
+    }
+
 
     public Vector2Int GetTileGridPosition(Vector2 mousePosition)
     {
@@ -28,4 +70,13 @@ public class ItemGrid : MonoBehaviour
 
         return tileGridPosition;
     }
+
+    public InventoryItem PickUpItem(Vector2Int tilePositionOnGrid)
+    {
+        InventoryItem pickedItem = inventoryItemGrid[tilePositionOnGrid.x, tilePositionOnGrid.y];
+        inventoryItemGrid[tilePositionOnGrid.x, tilePositionOnGrid.y] = null;
+        return pickedItem;
+    }
+
+
 }
